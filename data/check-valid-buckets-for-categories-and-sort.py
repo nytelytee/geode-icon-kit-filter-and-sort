91fb24bd6e0280dcfcd5bf008cecd8d663a03226
NyteLyte <nytelyte@tuta.io>
2026-02-27 03:01:06 +0100

    fix category order

--- data/check-valid-buckets-for-categories-and-sort.py
+++ data/check-valid-buckets-for-categories-and-sort.py
@@ -3,13 +3,13 @@ import sys
 
 VALID_CATEGORIES = [
     "Default Icons", "RobTop Levels", "User Levels", "Statistics",
-    "Shards", "Paths", "Map Packs", "Gauntlets", "List Rewards",
+    "Shards", "Paths", "Map Packs", "Gauntlets", "Upcoming Gauntlets", "List Rewards",
     "Daily Levels", "Creator", "Social", "Main Menu", "Vaults",
     "Shop", "Secret Shop", "Community Shop", "Diamond Shop",
     "Mechanic", "1 Key Chest", "5 Key Chest", "10 Key Chest",
     "25 Key Chest", "50 Key Chest", "100 Key Chest",
-    "Gold Key Chest", "Wraith Chest", "Special Chest",
-    "Unknown Wraith Chest", "Upcoming Gauntlets", "Supporter", "2.21"
+    "Gold Key Chest", "Wraith Chest", "Unknown Wraith Chest", "Special Chest",
+    "Supporter", "2.21"
 ]
 
 
